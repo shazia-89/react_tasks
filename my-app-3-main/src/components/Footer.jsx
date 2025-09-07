5416b5bcfecd171694072a949e1dae06f778c4ad
shazia-89 <shazia.azimi20@gmail.com>
2025-09-07 18:20:17 +0430

    first commit

---
+++ my-app-3-main/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer style={{ background: '#282c34', padding: '10px', color: 'white', marginTop: '20px' }}>
+      <p>© 2025 Shazia Azimi. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ my-app-3-main/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header style={{ background: '#282c34', padding: '10px', color: 'white' }}>
+      <h2>My React App</h2>
+    </header>
+  )
+}
+
+export default Header
